2d24ad4343b96d8045e6359923e97fae8b39675e
Maggie hum <hum.maggie@gmail.com>
2023-02-22 14:48:23 -0500

    review new/create done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,5 +5,20 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
